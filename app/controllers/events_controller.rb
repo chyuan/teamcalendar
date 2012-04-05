e629ca24c11368659d7437361ce571f4bd356a9c
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-04-05 11:39:13 -0500

    Group availability times now updates correctly, when a user deletes an event the table updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -127,11 +127,62 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     @users = User.all
-    @event.destroy
+ #   @event.destroy
 
     respond_to do |format|
+      
+         if @event.destroy
+    	if (@event.starttime.year == 2012 and @event.starttime.month == 4 and @event.starttime.day >= 8)
+    	  logger.debug "Event in range: #{@event.id}"
+    	  for day in @event.starttime.day..@event.endtime.day
+                if (@event.starttime.day == @event.endtime.day) 
+                  for hour in @event.starttime.hour..(@event.endtime.hour)
+    	        logger.debug "Event in range: #{day}"
+    	        logger.debug "Event in range: #{hour}"
+                    @hour = Hour.find_by_time(Time.utc(2012, 4, day, hour, 00, 00).in_time_zone)
+    	        logger.debug "hour id: #{@hour.id}"
+    	        @hour.numberbusy -= 1
+    	        @hour.save
+    		logger.debug "event's new numberbusy: #{@hour.numberbusy}"
+    	      end
+    	    elsif (day == @event.starttime.day)
+                  for hour in @event.starttime.hour..23
+    	        logger.debug "Event in range: #{day}"
+    	        logger.debug "Event in range: #{hour}"
+                    @hour = Hour.find_by_time(Time.utc(2012, 4, day, hour, 00, 00).in_time_zone)
+    	        logger.debug "hour id: #{@hour.id}"
+    	        @hour.numberbusy -= 1
+    	        @hour.save
+    		logger.debug "event's new numberbusy: #{@hour.numberbusy}"
+    	      end
+    	    elsif (day == @event.endtime.day)
+                  for hour in 0..(@event.endtime.hour + 1)
+    		logger.debug "Event in range: #{day}"
+    	        logger.debug "Event in range: #{hour}"
+                    @hour = Hour.find_by_time(Time.utc(2012, 4, day, hour, 00, 00).in_time_zone)
+    	        logger.debug "hour id: #{@hour.id}"
+    	        @hour.numberbusy -= 1
+    	        @hour.save
+    		logger.debug "event's new numberbusy: #{@hour.numberbusy}"
+    	      end
+    	    else
+                  for hour in 0..23
+    		logger.debug "Event in range: #{day}"
+    	        logger.debug "Event in range: #{hour}"
+                    @hour = Hour.find_by_time(Time.utc(2012, 4, day, hour, 00, 00).in_time_zone)
+    	        logger.debug "hour id: #{@hour.id}"
+    	        @hour.numberbusy -= 1
+    	        @hour.save
+    		logger.debug "event's new numberbusy: #{@hour.numberbusy}"
+    	      end
+    	    end
+    	  end
+end
+      
       format.html { redirect_to events_url }
       format.json { head :no_content }
     end
   end
+end
+
 end
