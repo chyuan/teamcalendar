637421feacfc42d20101e3fa2b8191b5c316e7f9
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-04-05 11:21:54 -0500

    Group availability updates correctly now when a user adds an event. Group availability is separated by days of the week.

--- app/controllers/free_times_controller.rb
+++ app/controllers/free_times_controller.rb
@@ -1,13 +1,13 @@
 class FreeTimesController < ApplicationController
   def index
     @hours = Hour.find_all_by_numberbusy(0)
-    @sunday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-08 08:00:00' AND time <= '2012-04-08 23:00:00'"])
-    @monday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-09 08:00:00' AND time <= '2012-04-09 23:00:00'"])
-    @tuesday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-10 08:00:00' AND time <= '2012-04-10 23:00:00'"])
-    @wednesday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-11 08:00:00' AND time <= '2012-04-11 23:00:00'"])
-    @thursday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-12 08:00:00' AND time <= '2012-04-12 23:00:00'"])
-    @friday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-13 08:00:00' AND time <= '2012-04-13 23:00:00'"])
-    @saturday = Hour.all(:conditions => ["(numberbusy = '0') AND time >= '2012-04-14 08:00:00' AND time <= '2012-04-14 23:00:00'"])
+    @sunday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '9' AND id <= '24'"])
+    @monday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '33' AND id <= '48'"])
+    @tuesday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '57' AND id <= '72'"])
+    @wednesday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '81' AND id <= '96'"])
+    @thursday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '105' AND id <= '120'"])
+    @friday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '129' AND id <= '144'"])
+    @saturday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '153' AND id <= '168'"])
     
     respond_to do |format|
       format.html # index.html.erb
@@ -17,5 +17,3 @@ class FreeTimesController < ApplicationController
 end
 
 
-
-#Hour.all(:conditions => ["(numberbusy = '1') AND time >= '2012-04-13 08:00:00' AND time <= '2012-04-13 23:00:00'"])
