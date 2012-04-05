ed8e31c3b953e32749a95d80d8d3fe14e7db4dcb
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-04-05 12:01:48 -0500

    Events are displayed in order after restoring them when a user deletes

--- app/controllers/free_times_controller.rb
+++ app/controllers/free_times_controller.rb
@@ -1,13 +1,14 @@
 class FreeTimesController < ApplicationController
   def index
     @hours = Hour.find_all_by_numberbusy(0)
-    @sunday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '9' AND id <= '24'"])
-    @monday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '33' AND id <= '48'"])
-    @tuesday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '57' AND id <= '72'"])
-    @wednesday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '81' AND id <= '96'"])
-    @thursday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '105' AND id <= '120'"])
-    @friday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '129' AND id <= '144'"])
-    @saturday = Hour.all(:conditions => ["numberbusy = '0' AND id >= '153' AND id <= '168'"])
+    @order = Hour.find(:all, :order => 'id')
+    @sunday = Hour.find(:all, :order => 'id', :conditions => ["numberbusy = '0' AND id >= '9' AND id <= '24'"])
+    @monday = Hour.find(:all, :order => 'id', :conditions => ["numberbusy = '0' AND id >= '33' AND id <= '48'"])
+    @tuesday = Hour.find(:all, :order => 'id', :conditions => ["numberbusy = '0' AND id >= '57' AND id <= '72'"])
+    @wednesday = Hour.find(:all, :order => 'id',:conditions => ["numberbusy = '0' AND id >= '81' AND id <= '96'"])
+    @thursday = Hour.find(:all, :order => 'id', :conditions => ["numberbusy = '0' AND id >= '105' AND id <= '120'"])
+    @friday = Hour.find(:all, :order => 'id', :conditions => ["numberbusy = '0' AND id >= '129' AND id <= '144'"])
+    @saturday = Hour.find(:all, :order => 'id',:conditions => ["numberbusy = '0' AND id >= '153' AND id <= '168'"])
     
     respond_to do |format|
       format.html # index.html.erb
